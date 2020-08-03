08d1b091cf8922140c8af45320fab1ed298af2c0
JongwonYoun <jeffyoun@naver.com>
2020-08-04 03:36:50 +0900

    FEAT: Button Component 추가

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import palette from '../../styles/palette';
+
+const ButtonBlock = styled.button`
+
+    outline : none;
+    border : none;
+
+    border-radius : 12.5px;
+    padding : 8.5px 27.5px;
+
+    margin : 10px;
+
+    -webkit-box-shadow: 10px 10px 20px -1px rgba(0,0,0,0.1);
+-moz-box-shadow: 10px 10px 20px -1px rgba(0,0,0,0.1);
+box-shadow: 10px 10px 20px -1px rgba(0,0,0,0.1);
+
+${(props: ButtonProps) =>
+  props.big &&
+  css`
+    padding: 31px;
+    font-size: 24px;
+    font-weight: 900;
+    width: 100%;
+  `}
+
+  ${(props: ButtonProps) =>
+    props.theme &&
+    css`
+      background-color: ${props.theme === 'white' ? palette.white : palette.gray};
+      color: ${props.theme === 'white' ? palette.mainRed : palette.darkGray};
+    `}
+
+  ${(props: ButtonProps) =>
+    props.selected &&
+    css`
+      background-color: ${palette.mainRed};
+      color: ${palette.white};
+    `}
+`;
+
+type Theme = 'white' | 'gray';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  big?: boolean;
+  selected?: boolean;
+  theme: Theme;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+function Button(props: ButtonProps) {
+  return <ButtonBlock {...props} />;
+}
+
+export default Button;
