2a7a39d3c975d9d9042eeee2a870c62f811599c3
JongwonYoun <jeffyoun@naver.com>
2020-08-04 04:09:30 +0900

    FEAT: style gray color 2개 추가

--- src/styles/palette.ts
+++ src/styles/palette.ts
@@ -2,6 +2,8 @@ const palette = {
   mainRed: '#DB2A37',
   gray: '#DDDDDD',
   lightGray: '#F7F7F7',
+  middleLightGray: '#ECECEC',
+  middleGray: '#8A8A8A',
   darkGray: '#5D5D5D',
   white: '#FFFFFF',
 };
