4dd8cc3294d6ee0b1078551d37bc00f0dda1b877
JongwonYoun <jeffyoun@naver.com>
2020-08-03 16:16:50 +0900

    FEAT: redux 환경 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './reset.scss';
 import * as serviceWorker from './serviceWorker';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
