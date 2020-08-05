59a00da2319eb842f9e18c9d89f7e5302b7eff96
JongwonYoun <jeffyoun@naver.com>
2020-08-05 21:05:07 +0900

    FIX: Loader가 세로 중앙에 제대로 오도록 수정

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -4,13 +4,17 @@ import PacmanLoader from 'react-spinners/PacmanLoader';
 import palette from '../../styles/palette';
 
 const LoaderBlock = styled.div`
-  height: 100%;
-  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
 
-  margin-top: 40%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -100px;
+  margin-left: -100px;
+  width: 200px;
+  height: 200px;
   transform: translateX(-30px);
 `;
 
