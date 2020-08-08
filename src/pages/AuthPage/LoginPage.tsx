3815e4c3070379d3453a9b09bbd57d3d57aedc7e
JongwonYoun <jeffyoun@naver.com>
2020-08-08 18:57:08 +0900

    FIX: 개발환경에 따라 콜백 주소 다르게

--- src/pages/AuthPage/LoginPage.tsx
+++ src/pages/AuthPage/LoginPage.tsx
@@ -6,14 +6,14 @@ import Header from '../../components/layout/Header';
 
 const LoginPageBlock = styled.div``;
 
+const redirectionURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/auth/kakao/callback' : 'http://221.149.10.240:5000/auth/kakao/callback';
+
 function LoginPage({ history }: RouteComponentProps) {
   return (
     <Container color="white">
       <Header category="modal" headerColor="white" />
       <a
-        href={`https://kauth.kakao.com/oauth/authorize?client_id=${
-          process.env.REACT_APP_KAKAO_REST_API_KEY
-        }&redirect_uri=${'http://localhost:3000/auth/kakao/callback'}&response_type=code`}
+        href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${redirectionURL}&response_type=code`}
       >
         카카오 로그인
       </a>
