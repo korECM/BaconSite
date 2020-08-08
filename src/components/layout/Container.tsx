9aee6eaa54552354b3cfddc7f7063006a20ad9cd
JongwonYoun <jeffyoun@naver.com>
2020-08-08 17:27:58 +0900

    FEAT: Container width 어느정도 반응형

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -4,11 +4,34 @@ import palette from '../../styles/palette';
 
 const ContainerBlock = styled.div`
   padding: 0 7.5%;
+
   padding-top: 35px;
 
   padding-bottom: 50px;
 
   height: 100%;
+
+  @media only screen and (min-width: 768px) {
+    padding: 0 15%;
+    padding-top: 35px;
+
+    padding-bottom: 50px;
+  }
+
+  @media only screen and (min-width: 1000px) {
+    padding: 0 27.5%;
+    padding-top: 35px;
+
+    padding-bottom: 50px;
+  }
+
+  @media only screen and (min-width: 1600px) {
+    padding: 0 35%;
+    padding-top: 35px;
+
+    padding-bottom: 50px;
+  }
+
   ${(props: ContainerProps) =>
     css`
       color: ${props.color === 'red' ? palette.white : palette.mainRed};
