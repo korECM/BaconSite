30a1f4ae001f36470f04ac11c9ee53d772e60ba1
JongwonYoun <jeffyoun@naver.com>
2020-08-12 19:35:59 +0900

    FEAT: Dialog 애니메이션 추가

--- src/components/common/Dialog.tsx
+++ src/components/common/Dialog.tsx
@@ -1,9 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import React from 'react';
 import Button from './Button';
 import ButtonGroup from './ButtonGroup';
+import { useTransition, animated } from 'react-spring';
 
-const DialogBlock = styled.div`
+const DialogBlock = styled(animated.div)`
   position: fixed;
   top: 0;
   left: 0;
@@ -15,7 +16,7 @@ const DialogBlock = styled.div`
   justify-content: center;
 `;
 
-const DialogBackground = styled.div`
+const DialogBackground = styled(animated.div)`
   position: fixed;
   top: 0;
   left: 0;
@@ -37,12 +38,13 @@ const WhiteBox = styled.div`
     font-size: 1.5rem;
     color: #343a40;
     margin-top: 0;
-    margin-bottom: 1rem;
+    margin-bottom: 2rem;
   }
 
   p {
     font-size: 1.125rem;
     margin: 0;
+    margin-bottom: 2rem;
     color: #868e96;
   }
 `;
@@ -55,35 +57,65 @@ interface DialogProps {
   confirmText?: string;
   onCancel: () => void;
   onConfirm?: () => void;
+  visible: boolean;
 }
 
-const Dialog = (props: DialogProps) => {
+const Dialog = (p: DialogProps) => {
+  const fadeTransition = useTransition(p.visible, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  const slideUpTransition = useTransition(p.visible, null, {
+    from: {
+      transform: `translateY(200px) scale(0.8)`,
+      opacity: 0,
+    },
+    enter: {
+      transform: `translateY(0px) scale(1)`,
+      opacity: 1,
+    },
+    leave: {
+      transform: `translateY(200px) scale(0.8)`,
+      opacity: 0,
+    },
+    config: {
+      tension: 200,
+      friction: 15,
+    },
+  });
+
   return (
     <div>
-      <DialogBackground />
-      <DialogBlock>
-        <WhiteBox>
-          <h3>{props.title}</h3>
-          <p>{props.desc}</p>
-          {props.mode === 'confirm' && (
-            <ButtonGroup direction="row" rightAlign gap="0">
-              <Button theme="white" onClick={props.onConfirm}>
-                {props.confirmText}
-              </Button>
-            </ButtonGroup>
-          )}
-          {props.mode === 'cancel' && (
-            <ButtonGroup direction="row" rightAlign gap="10px">
-              <Button theme="text" onClick={props.onCancel}>
-                {props.cancelText}
-              </Button>
-              <Button theme="red" onClick={props.onConfirm}>
-                {props.confirmText}
-              </Button>
-            </ButtonGroup>
-          )}
-        </WhiteBox>
-      </DialogBlock>
+      {fadeTransition.map(({ item, key, props }) => (item ? <DialogBackground key={key} style={props} /> : null))}
+      {slideUpTransition.map(({ item, key, props }) =>
+        item ? (
+          <DialogBlock style={props} key={key}>
+            <WhiteBox>
+              <h3>{p.title}</h3>
+              <p>{p.desc}</p>
+              {p.mode === 'confirm' && (
+                <ButtonGroup direction="row" rightAlign gap="0">
+                  <Button theme="white" onClick={p.onConfirm}>
+                    {p.confirmText}
+                  </Button>
+                </ButtonGroup>
+              )}
+              {p.mode === 'cancel' && (
+                <ButtonGroup direction="row" rightAlign gap="10px">
+                  <Button theme="text" onClick={p.onCancel}>
+                    {p.cancelText}
+                  </Button>
+                  <Button theme="red" onClick={p.onConfirm}>
+                    {p.confirmText}
+                  </Button>
+                </ButtonGroup>
+              )}
+            </WhiteBox>
+          </DialogBlock>
+        ) : null,
+      )}
     </div>
   );
 };
