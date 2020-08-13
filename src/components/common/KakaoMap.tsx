0c59d894c64d5b2cfa6c26528151121c5d8fdfc5
JongwonYoun <jeffyoun@naver.com>
2020-08-13 23:23:56 +0900

    FIX: 카카오맵 확대 정도 수정

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -18,7 +18,7 @@ function KakaoMap({ latitude, longitude }: KakaoMapProps) {
     let options = {
       //지도를 생성할 때 필요한 기본 옵션
       center: new window.kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
+      level: 2, //지도의 레벨(확대, 축소 정도)
     };
 
     let map = new window.kakao.maps.Map(container!, options); //지도 생성 및 객체 리턴
