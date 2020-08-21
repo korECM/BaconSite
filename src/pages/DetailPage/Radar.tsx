32d73b2a398f4913cf15bdf0dea4aadbc3ca767f
JongwonYoun <jeffyoun@naver.com>
2020-08-22 03:55:13 +0900

    FEAT: Radara Lazy Loading

--- src/pages/DetailPage/Radar.tsx
+++ src/pages/DetailPage/Radar.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Radar as RadarChart } from 'react-chartjs-2';
+import React, { Suspense } from 'react';
 import styled from 'styled-components';
 import { ShopUIInterface } from '../../modules/detail';
 import palette, { hexToRGB } from '../../styles/palette';
+import { GridLoader } from 'react-spinners';
 
 const RadarContainer = styled.div`
   background-color: white;
@@ -15,9 +15,18 @@ const RadarContainer = styled.div`
   box-shadow: 5px 5px 20px -1px rgba(0, 0, 0, 0.1);
 `;
 
+const Loader = styled.div`
+  height: 220px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 interface RadarProps {
   shop: ShopUIInterface;
 }
+
+const RadarChart = React.lazy(() => import('./RadarProxy'));
 
 function Radar({ shop }: RadarProps) {
   let max =
@@ -26,46 +35,54 @@ function Radar({ shop }: RadarProps) {
 
   return (
     <RadarContainer>
-      <RadarChart
-        height={220}
-        data={{
-          labels: ['분위기', '가성비', '단체', '혼밥', '밥약', '매워요'],
-          datasets: [
-            {
-              data: [
-                shop.keyword.atmosphere / max,
-                shop.keyword.costRatio / max,
-                shop.keyword.group / max,
-                shop.keyword.individual / max,
-                shop.keyword.riceAppointment / max,
-                shop.keyword.spicy / max,
-              ],
-              borderColor: hexToRGB(palette.mainRed, 0.8),
-              borderWidth: 1.5,
-              backgroundColor: hexToRGB(palette.mainRed, 0.2),
-              pointRadius: 0,
-              pointBackgroundColor: hexToRGB(palette.mainRed, 0.8),
-            },
-          ],
-        }}
-        options={{
-          legend: {
-            display: false,
-          },
-          scale: {
-            ticks: {
+      <Suspense
+        fallback={
+          <Loader>
+            <GridLoader color={palette.mainRed} />
+          </Loader>
+        }
+      >
+        <RadarChart
+          height={220}
+          data={{
+            labels: ['분위기', '가성비', '단체', '혼밥', '밥약', '매워요'],
+            datasets: [
+              {
+                data: [
+                  shop.keyword.atmosphere / max,
+                  shop.keyword.costRatio / max,
+                  shop.keyword.group / max,
+                  shop.keyword.individual / max,
+                  shop.keyword.riceAppointment / max,
+                  shop.keyword.spicy / max,
+                ],
+                borderColor: hexToRGB(palette.mainRed, 0.8),
+                borderWidth: 1.5,
+                backgroundColor: hexToRGB(palette.mainRed, 0.2),
+                pointRadius: 0,
+                pointBackgroundColor: hexToRGB(palette.mainRed, 0.8),
+              },
+            ],
+          }}
+          options={{
+            legend: {
               display: false,
-              stepSize: 0.2,
             },
-            gridLines: {
-              // color: 'transparent',
+            scale: {
+              ticks: {
+                display: false,
+                stepSize: 0.2,
+              },
+              gridLines: {
+                // color: 'transparent',
+              },
+              angleLines: {
+                // color: 'transparent',
+              },
             },
-            angleLines: {
-              // color: 'transparent',
-            },
-          },
-        }}
-      />
+          }}
+        />
+      </Suspense>
     </RadarContainer>
   );
 }
