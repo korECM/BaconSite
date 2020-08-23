a65745581c274326e5d6f62562a81a0f0da35635
hyunju <dlguswn3659@naver.com>
2020-08-24 00:37:59 +0900

    ADD: filtering 클릭

--- src/pages/FilterPage/FilterPage.tsx
+++ src/pages/FilterPage/FilterPage.tsx
@@ -9,8 +9,9 @@ import RestaurantCard from '../../components/common/RestaurantCard';
 import Loader from '../../components/common/Loader';
 import { Fade, Bounce } from 'react-awesome-reveal';
 import { Animated } from 'react-animated-css';
-import React from 'react';
+import React, { Component } from 'react';
 import { Helmet } from 'react-helmet-async';
+import './TagButton.css';
 
 const TitleComment = styled.h1`
   font-family: 'Nanum Gothic';
@@ -44,203 +45,153 @@ const ButtonLine = styled.h1`
   vertical-align: center;
 `;
 
-const ButtonBlock = styled.button`
-  outline: none;
-  border: none;
-
-  border-radius: 10px;
-
-  width: 27%;
-  height: 90px;
-
-  margin: 3%;
-
-  background-color: ${palette.mainRed};
-  color: ${palette.white};
-
-  font-size: 10px;
-  font-family: 'Nanum Gothic';
-  font-weight: 200;
-
-  -webkit-box-shadow: 10px 10px 20px -1px rgba(0, 0, 0, 0.1);
-  -moz-box-shadow: 10px 10px 20px -1px rgba(0, 0, 0, 0.1);
-  box-shadow: 10px 10px 20px -1px rgba(0, 0, 0, 0.1);
-`;
-
-const TextBox = styled.h1`
-  font-size: 10px;
-  font-family: 'Nanum Gothic';
-  font-weight: 200;
-  color: white;
-  padding-bottom: 3px;
-`;
-
 const Divider = styled.div`
   border-bottom: 0px solid rgba(138, 138, 138, 0.5);
   margin-bottom: 17px;
 `;
 
-const ButtonBlock2 = styled.button`
+interface Props extends RouteComponentProps {}
 
-    outline : none;
-    border : none;
-
-    border-radius : 12.5px;
-    padding : 8.5px 27.5px;
-
-    margin-bottom: 15px;
-    margin-top: 15px;
-    margin: 3%;
-
-
-
-  transition : background-color 0.2s ease;
-
-  ${(props: ButtonProps) =>
-    props.fullWidth &&
-    css`
-      width: 100%;
-    `}
-
-  ${(props: ButtonProps) =>
-    !['text', 'border'].includes(props.theme) &&
-    css`
-      -webkit-box-shadow: 10px 10px 20px -1px rgba(0, 0, 0, 0.1);
-      -moz-box-shadow: 10px 10px 20px -1px rgba(0, 0, 0, 0.1);
-      box-shadow: 10px 10px 20px -1px rgba(0, 0, 0, 0.1);
-      :hover {
-        background-color: ${palette.mainRed};
-        color: ${palette.white};
-        cursor: pointer;
-      }
-    `}
-
-    ${(props: ButtonProps) =>
-      props.theme === 'red' &&
-      css`
-        :hover {
-          background-color: ${hexToRGB(palette.mainRed, 0.7)};
-          color: ${palette.white};
-        }
-      `}
-
-  ${(props: ButtonProps) =>
-    props.middle &&
-    css`
-      padding: 15px;
-      border-radius: 30px;
-      font-size: 16px;
-      font-weight: 900;
-    `}
-
-${(props: ButtonProps) =>
-  props.big &&
-  css`
-    padding: 31px;
-    font-size: 24px;
-    font-weight: 900;
-    width: 100%;
-  `}
-
-  ${(props: ButtonProps) =>
-    props.theme === 'white' &&
-    css`
-      background-color: ${palette.white};
-      color: ${palette.mainRed};
-    `}
-    ${(props: ButtonProps) =>
-      props.theme === 'red' &&
-      css`
-        background-color: ${palette.mainRed};
-        color: ${palette.white};
-      `}
-    ${(props: ButtonProps) =>
-      props.theme === 'gray' &&
-      css`
-        background-color: ${palette.gray};
-        color: ${palette.darkGray};
-      `}
-    ${(props: ButtonProps) =>
-      props.theme === 'text' &&
-      css`
-        background-color: transparent;
-        color: ${palette.mainRed};
-      `}
-
-      ${(props: ButtonProps) =>
-        props.theme === 'border' &&
-        css`
-          background-color: transparent;
-          color: ${palette.mainRed};
-          border: 1px solid ${palette.mainRed};
-        `}
-
-  ${(props: ButtonProps) =>
-    props.selected &&
-    css`
-      background-color: ${palette.mainRed};
-      color: ${palette.white};
-    `}
-
-  ${(props: ButtonProps) =>
-    props.disabled &&
-    css`
-      background-color: ${palette.middleGray};
-      color: ${palette.white};
-      &:hover {
-        background-color: ${palette.middleGray};
-        color: ${palette.white};
-        cursor: default;
-      }
-    `}
-`;
-
-type Theme = 'white' | 'gray' | 'text' | 'red' | 'border';
-// type Selected = 'true' | 'false';
-// let Selected = 'false';
-
-interface ButtonProps {
-  children: React.ReactNode;
-  big?: boolean;
-  middle?: boolean;
-  selected?: boolean;
-  disabled?: boolean;
-  theme: Theme;
-  fullWidth?: boolean;
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  onMouseOver?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+interface State {
+  sorting_bool: boolean[];
+  sortings: string[];
+  food_bool: boolean[];
+  foods: string[];
+  price_bool: boolean[];
+  prices: string[];
+  place_bool: boolean[];
+  places: string[];
+  keyword_bool: boolean[];
+  keywords: string[];
 }
 
-function Button(props: ButtonProps) {
-  return <ButtonBlock2 {...props} />;
-}
+class FilterPage extends React.Component<Props, State> {
+  state: State = {
+    sorting_bool: [false, false, false],
+    sortings: ['별점순', '최신순', '리뷰순'],
+    food_bool: [false, false, false, false, false, false],
+    foods: ['한식', '중식', '일식', '양식', '분식', 'ALL'],
+    price_bool: [false, false, false, false],
+    prices: ['5천원 이하', '1만원 이하', '2만원 이하', '3만원 이하'],
+    place_bool: [false, false, false],
+    places: ['정문', '후문', 'ALL'],
+    keyword_bool: [false, false, false, false, false, false],
+    keywords: ['가성비', '분위기', '단체', '혼밥', '밥약', '맵찔'],
+  };
 
-function FilterPage({ match }: RouteComponentProps) {
-  return (
-    <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
-      <Fade>
-        <Container color="white">
-          <Helmet>
-            <title>당신만을 위한 식당 - 푸딩</title>
-          </Helmet>
-          <Header category="modal" headerColor="red" />
-          <Fade>
-            <Bounce>
-              <TitleComment>필터링 검색</TitleComment>
-              <SubtitleComment>정렬</SubtitleComment>
-              <ButtonLine>
-                <Button theme="gray">추천순</Button>
-                <Button theme="gray">평점순</Button>
-                <Button theme="gray">리뷰순</Button>
-              </ButtonLine>
-              <Divider></Divider>
-              <ButtonLine></ButtonLine>
-            </Bounce>
-          </Fade>
-        </Container>
-      </Fade>
-    </Animated>
-  );
+  changeColor(i: number) {
+    console.log(i);
+    this.setState({
+      sorting_bool: this.state.sorting_bool.map((item, index) => (index !== i ? item : !item)),
+    });
+  }
+
+  render() {
+    const { sortings, foods, prices, places, keywords } = this.state;
+
+    const sortingList = sortings.map((sorting, i) => (
+      <button
+        className={`roundButton yHover ${this.state.sorting_bool[i] ? 'mainred' : ''}`}
+        key={sorting}
+        onClick={(e) => this.changeColor(i)}
+        style={{ height: 35, flex: 1 }}
+      >
+        <div style={TagTextStyle}>{sorting}</div>
+      </button>
+    ));
+
+    const foodList = foods.map((food, i) => (
+      <button
+        className={`roundButton yHover ${this.state.food_bool[i] ? 'mainred' : ''}`}
+        key={food}
+        onClick={(e) => this.changeColor(i)}
+        style={{ height: 35, flex: 1 }}
+      >
+        <div style={TagTextStyle}>{food}</div>
+      </button>
+    ));
+
+    const priceList = prices.map((price, i) => (
+      <button
+        className={`roundButton yHover ${this.state.price_bool[i] ? 'mainred' : ''}`}
+        key={price}
+        onClick={(e) => this.changeColor(i)}
+        style={{ height: 35, flex: 1 }}
+      >
+        <div style={TagTextStyle}>{price}</div>
+      </button>
+    ));
+
+    const placeList = places.map((place, i) => (
+      <button
+        className={`roundButton yHover ${this.state.place_bool[i] ? 'mainred' : ''}`}
+        key={place}
+        onClick={(e) => this.changeColor(i)}
+        style={{ height: 35, flex: 1 }}
+      >
+        <div style={TagTextStyle}>{place}</div>
+      </button>
+    ));
+
+    const keywordList = keywords.map((keyword, i) => (
+      <button
+        className={`roundButton yHover ${this.state.keyword_bool[i] ? 'mainred' : ''}`}
+        key={keyword}
+        onClick={(e) => this.changeColor(i)}
+        style={{ height: 35, flex: 1 }}
+      >
+        <div style={TagTextStyle}>{keyword}</div>
+      </button>
+    ));
+
+    var TagTextStyle = {
+      fontSize: 30,
+      flex: 1,
+    };
+
+    return (
+      <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+        <Fade>
+          <Container color="white">
+            <Helmet>
+              <title>당신만을 위한 식당 - 푸딩</title>
+            </Helmet>
+            <Header category="modal" headerColor="red" />
+            <Fade>
+              <Bounce>
+                <TitleComment>필터링 검색</TitleComment>
+                <SubtitleComment>정렬</SubtitleComment>
+                <ButtonLine>
+                  <div style={TagTextStyle}>{sortingList}</div>
+                </ButtonLine>
+                <Divider></Divider>
+                <SubtitleComment>음식종류</SubtitleComment>
+                <ButtonLine>
+                  <div style={TagTextStyle}>{foodList}</div>
+                </ButtonLine>
+                <Divider></Divider>
+                <SubtitleComment>최저가격대</SubtitleComment>
+                <ButtonLine>
+                  <div style={TagTextStyle}>{priceList}</div>
+                </ButtonLine>
+                <Divider></Divider>
+                <SubtitleComment>위치</SubtitleComment>
+                <ButtonLine>
+                  <div style={TagTextStyle}>{placeList}</div>
+                </ButtonLine>
+                <Divider></Divider>
+                <SubtitleComment>키워드</SubtitleComment>
+                <ButtonLine>
+                  <div style={TagTextStyle}>{keywordList}</div>
+                </ButtonLine>
+                <Divider></Divider>
+              </Bounce>
+            </Fade>
+          </Container>
+        </Fade>
+      </Animated>
+    );
+  }
 }
 
 export default FilterPage;
