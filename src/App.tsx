ebb07f2919a03eb00b2fe90afa2ce2d56f4fe75f
JongwonYoun <jeffyoun@naver.com>
2020-08-25 17:43:33 +0900

    REFACT: 페이지 로딩 최적화

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Loader from './components/common/Loader';
 import HomePage from './pages/HomePage';
-import ResultPage from './pages/ResultPage/ResultPage';
 import Container from './components/layout/Container';
 import Header from './components/layout/Header';
+import Loader from './components/common/Loader';
+import DetailPageRouter from './pages/DetailPage/DetailPageRouter';
 import { Helmet } from 'react-helmet-async';
 
-const DetailPageRouter = React.lazy(() => import(/* webpackChunkName: "result", webpackPrefetch: true */ './pages/DetailPage/DetailPageRouter'));
+const ResultPage = React.lazy(() => import(/* webpackChunkName: "result", webpackPrefetch: true */ './pages/ResultPage/ResultPage'));
 const AdminRouter = React.lazy(() => import('./pages/AdminPage/AdminRouter'));
 const RouletteRouter = React.lazy(() => import(/* webpackChunkName: "roulette", webpackPrefetch: true */ './pages/RoulettePage/RoulettePageRouter'));
 const RouletteListRouter = React.lazy(() => import(/* webpackChunkName: "rouletteList", webpackPrefetch: true */ './pages/RoulettePage/RouletteListRouter'));
