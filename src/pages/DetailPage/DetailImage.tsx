ae0097d4f3799f61e9cf425b0256d1f587814d96
JongwonYoun <jeffyoun@naver.com>
2020-08-30 20:55:19 +0900

    FEAT: 일단 사진 크게 보기 기능만 추가

--- src/pages/DetailPage/DetailImage.tsx
+++ src/pages/DetailPage/DetailImage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import styled from 'styled-components';
 import Container from 'components/layout/Container';
 import Header from 'components/layout/Header';
@@ -6,12 +6,15 @@ import { ShopUIInterface } from 'modules/detail';
 import useDetail from 'hooks/useDetail';
 import { RouteComponentProps } from 'react-router-dom';
 import palette from 'styles/palette';
+import { Image } from 'api/getShop';
 
 const DetailImageBlock = styled.div`
-  h1 {
-    font-size: 31px;
-    font-weight: bolder;
-    margin: 15px 0;
+  .imageHeader {
+    h1 {
+      font-size: 31px;
+      font-weight: bolder;
+      margin: 15px 0;
+    }
   }
   height: 100%;
 `;
@@ -30,8 +33,17 @@ const ImageContainer = styled.div`
 
   img {
     width: 100%;
-    height: 100px;
+    height: 15vh;
     object-fit: cover;
+    cursor: pointer;
+  }
+`;
+
+const ImageViewer = styled.div`
+  height: 100%;
+  background-color: black;
+  img {
+    width: 100%;
   }
 `;
 
@@ -42,20 +54,44 @@ function DetailImage({ match }: DetailImageProps) {
 
   const { shop, onShopRequest } = useDetail(shopId);
 
+  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
+
   useEffect(() => {
     onShopRequest();
   }, [onShopRequest]);
 
+  const onImageClick = useCallback((image: Image) => {
+    setSelectedImage(image);
+  }, []);
+
+  const onImageClose = useCallback(() => {
+    setSelectedImage(null);
+  }, []);
+
   return (
     <DetailImageBlock>
       <Container color="white">
-        <Header category="modal" headerColor="red" />
-        <h1>{shop.data?.name}</h1>
-        <ImageContainer>
-          {shop.data?.shopImage.map((image) => (
-            <img src={image.imageLink} alt="가게 사진" key={image._id} />
-          ))}
-        </ImageContainer>
+        <Header category="modal" headerColor="white" />
+        <div className="imageHeader">
+          <h1>{shop.data?.name}</h1>
+        </div>
+        {selectedImage ? (
+          <ImageViewer>
+            <img src={selectedImage.imageLink} alt="가게 사진" onClick={onImageClose} />
+          </ImageViewer>
+        ) : (
+          <ImageContainer>
+            {shop.data?.shopImage.map((image) => (
+              <img src={image.imageLink} alt="가게 사진" key={image._id} onClick={() => onImageClick(image)} />
+            ))}
+            {shop.data?.shopImage.map((image) => (
+              <img src={image.imageLink} alt="가게 사진" key={image._id} onClick={() => onImageClick(image)} />
+            ))}
+            {shop.data?.shopImage.map((image) => (
+              <img src={image.imageLink} alt="가게 사진" key={image._id} onClick={() => onImageClick(image)} />
+            ))}
+          </ImageContainer>
+        )}
       </Container>
     </DetailImageBlock>
   );
