761f3bcf728548cfe196cb3c4b756a1736de8875
JongwonYoun <jeffyoun@naver.com>
2020-09-12 20:03:37 +0900

    FEAT: jennifersoft, ga 추가

--- src/index.tsx
+++ src/index.tsx
@@ -10,14 +10,31 @@ import rootReducer, { RootState } from './modules';
 import { BrowserRouter } from 'react-router-dom';
 import { setUser, checkThunk, checkAsync } from './modules/user';
 import { UserInterface } from './api/auth';
-
+import ReactGA from 'react-ga';
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(Thunk)));
 
 function loadUser() {
   try {
     // TODO: localStorage는 없고 쿠키만 있는 경우 로그인 안되는 오류 존재
     const user = localStorage.getItem('user');
-    if (!user) return;
+    if (!user) {
+      const trackingId = 'UA-177861548-1'; // Replace with your Google Analytics tracking ID
+      ReactGA.initialize(trackingId);
+      ReactGA.set({
+        userId: 'anonymous',
+        // any data that is relevant to the user session
+        // that you would like to track with google analytics
+      });
+      return;
+    }
+
+    const trackingId = 'UA-177861548-1'; // Replace with your Google Analytics tracking ID
+    ReactGA.initialize(trackingId);
+    ReactGA.set({
+      userId: (user as any).name,
+      // any data that is relevant to the user session
+      // that you would like to track with google analytics
+    });
 
     store.dispatch(setUser(JSON.parse(user) as UserInterface));
     (store.dispatch as ThunkDispatch<
