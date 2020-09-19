81380b44343c9c7c37290a504549ba537ddcbe74
JongwonYoun <jeffyoun@naver.com>
2020-09-20 00:23:02 +0900

    FEAT: report get api 추가, report router 변경

--- src/api/report.ts
+++ src/api/report.ts
@@ -10,9 +10,70 @@ export interface ReportShopInterface {
   comment: string;
 }
 
+export enum ShopReportState {
+  Issued = 'issued',
+  Confirmed = 'confirmed',
+  Rejected = 'rejected',
+  Done = 'done',
+}
+export enum ReviewReportState {
+  Issued = 'issued',
+  Rejected = 'rejected',
+  Done = 'done',
+}
+
+export enum ImageReportState {
+  Issued = 'issued',
+  Rejected = 'rejected',
+  Done = 'done',
+}
+
+export interface ShopReportResponse {
+  comment: string;
+  type: number[];
+  shopId: string;
+  userId: string;
+  registerDate: Date;
+  state: ShopReportState;
+}
+
+export interface ReviewReportResponse {
+  comment: string;
+  reviewId: string;
+  userId: string;
+  registerDate: Date;
+  state: ReviewReportState;
+}
+
+export interface ImageReportResponse {
+  imageId: string;
+  userId: string;
+  registerDate: Date;
+  state: ImageReportState;
+}
+
+export async function getShopReportAPI() {
+  const response = await axios.get<ShopReportResponse>(apiLink() + `/report/shop`, {
+    withCredentials: true,
+  });
+  return response.data;
+}
+export async function getReviewReportAPI() {
+  const response = await axios.get<ReviewReportResponse>(apiLink() + `/report/review`, {
+    withCredentials: true,
+  });
+  return response.data;
+}
+export async function getImageReportAPI() {
+  const response = await axios.get<ImageReportResponse>(apiLink() + `/report/image`, {
+    withCredentials: true,
+  });
+  return response.data;
+}
+
 export async function reportShopAPI(shopId: string, data: ReportShopInterface) {
   const response = await axios.post<ReportInterface>(
-    apiLink() + `/shop/report/${shopId}`,
+    apiLink() + `/report/shop/${shopId}`,
     {
       type: data.type.map((t, i) => (t ? i : -1)).filter((t) => t >= 0),
       comment: data.comment,
@@ -26,7 +87,7 @@ export async function reportShopAPI(shopId: string, data: ReportShopInterface) {
 
 export async function reportReviewAPI(reviewId: string, comment: string) {
   const response = await axios.post<ReportInterface>(
-    apiLink() + `/shop/review/report/${reviewId}`,
+    apiLink() + `/report/review/${reviewId}`,
     {
       comment,
     },
@@ -39,7 +100,7 @@ export async function reportReviewAPI(reviewId: string, comment: string) {
 
 export async function reportImageAPI(imageId: string) {
   const response = await axios.post<ReportInterface>(
-    apiLink() + `/shop/report/image/${imageId}`,
+    apiLink() + `/report/image/${imageId}`,
     {},
     {
       withCredentials: true,
