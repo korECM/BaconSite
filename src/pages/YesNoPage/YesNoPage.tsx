3cffc11584857b6ed7a74abe768835a7dce1709c
hyunju <dlguswn3659@naver.com>
2020-09-20 01:23:29 +0900

    FIX: Yesno 멘트 수정

--- src/pages/YesNoPage/YesNoPage.tsx
+++ src/pages/YesNoPage/YesNoPage.tsx
@@ -39,22 +39,22 @@ const base = [
   },
   {
     id: 5,
-    name: '정문 근처',
+    name: '현재 R&D관과 가까움',
     img: 'wondering_cat.png',
   },
   {
     id: 6,
-    name: '후문 근처',
+    name: '현재 310관과 가까움',
     img: 'wondering_cat.png',
   },
   {
     id: 7,
-    name: '혼밥',
+    name: '요즘 대세인 혼밥',
     img: 'wondering_cat.png',
   },
   {
     id: 8,
-    name: '혼밥 아님',
+    name: '시대에 뒤떨어진 합밥',
     img: 'wondering_cat.png',
   },
   {
