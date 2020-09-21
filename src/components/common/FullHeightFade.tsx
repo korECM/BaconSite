1221cbbc2a88d3b4382b1a9cd5ad791fe21edae1
JongwonYoun <jeffyoun@naver.com>
2020-09-22 04:07:57 +0900

    FIX: 사파리 animation 문제 해결

--- src/components/common/FullHeightFade.tsx
+++ src/components/common/FullHeightFade.tsx
@@ -8,46 +8,10 @@ interface FullHeightProps {
 const Fade = styled.div`
   height: 100%;
 
-  animation: fadein 0.5s;
-  -moz-animation: fadein 0.5s; /* Firefox */
-  -webkit-animation: fadein 0.5s; /* Safari and Chrome */
-  -o-animation: fadein 0.5s; /* Opera */
-
-  @keyframes fadein {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-  @-moz-keyframes fadein {
-    /* Firefox */
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-  @-webkit-keyframes fadein {
-    /* Safari and Chrome */
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-  @-o-keyframes fadein {
-    /* Opera */
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
+  animation-name: fadeIn;
+  animation-duration: 0.5s;
+  -webkit-animation-name: fadeIn;
+  -webkit-animation-duration: 0.5s;
 `;
 
 function FullHeightFade({ children }: FullHeightProps) {
