7382c5dc25967562be846fb0071f4487f9c464b0
JongwonYoun <jeffyoun@naver.com>
2020-09-30 23:29:04 +0900

    FIX: useState 안한거 수정

--- src/pages/AuthPage/KakaoPage.tsx
+++ src/pages/AuthPage/KakaoPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import cx from 'classnames';
 import { Redirect, RouteComponentProps } from 'react-router-dom';
