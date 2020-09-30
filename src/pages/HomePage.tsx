75402f235bf8ea2a44da2e08699fd7433c6c9f13
JongwonYoun <jeffyoun@naver.com>
2020-09-30 14:36:54 +0900

    REFACT: HomePage 함수형 컴포넌트로 변경

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '../components/layout/Container';
 import Header from '../components/layout/Header';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import RoundContainer from '../components/common/RoundContainer';
 import { RouteComponentProps } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import palette from '../styles/palette';
-// import firstbutton from 'assets/firstbutton.jpg';
-// import secondbutton from 'assets/secondbutton.jpg';
-// import thirdbutton from 'assets/thirdbutton.jpg';
-// import toustousbutton from 'assets/toustousbutton.jpg';
 import search from 'assets/search.png';
 import titlelogo from 'assets/fooding_logo_outline.svg';
 import './TagButton.css';
@@ -225,128 +221,90 @@ const AddText = styled.div`
   }
 `;
 
-interface Props extends RouteComponentProps {}
+function HomePage({ history }: RouteComponentProps) {
+  const [input, setInput] = useState('');
 
-interface RouletteItemState {
-  id: number;
-  text: string;
-  done: boolean;
-}
-
-interface DataInterface {
-  name: string;
-}
-
-interface State {
-  input: string;
-}
-
-let beClicked = false;
-let selected_name = 'false';
-
-class HomePage extends React.Component<Props, State> {
-  state: State = {
-    input: '',
-  };
-
-  onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    const { input } = this.state;
     if (input !== '' && input.length < 10) {
       //10글자로 제한
-      this.setState({
-        input: '',
-      });
+      setInput('');
     }
-    console.log('push search button');
   };
 
-  onChange = (e: React.FormEvent<HTMLInputElement>): void => {
+  const onChange = (e: React.FormEvent<HTMLInputElement>): void => {
     const { value } = e.currentTarget;
-    this.setState({
-      input: value,
-    });
+    setInput(value);
   };
 
-  moveHref = (data: DataInterface) => {
-    beClicked = true;
-    selected_name = 'true';
-    if (data.name !== '') {
-      this.props.history.push({
+  const moveHref = (input: string) => {
+    if (input !== '') {
+      history.push({
         pathname: '/result',
-        search: 'name=' + data.name + '&search=true',
+        search: 'name=' + input + '&search=true',
       });
     }
   };
 
-  render() {
-    const { onSubmit, onChange, moveHref } = this;
-    const { input } = this.state;
-
-    let data: DataInterface = {
-      name: input,
-    };
-
-    return (
-      <>
-        <Container color="white">
-          <Header category="main" headerColor="none" withOutHeaderIcon />
-          <TitleSlogan>뭐 먹을지 고민될 땐?</TitleSlogan>
-          <TitleLogo>
-            <img src={titlelogo} alt="titlelogo" />
-          </TitleLogo>
-          <Divider></Divider>
-          <ButtonLine>
-            <ButtonBlock>
-              <Link to="/filter">
-                <TextBox>필터링</TextBox>
-                <TextBox>검색</TextBox>
-              </Link>
-            </ButtonBlock>
-            <ButtonBlock>
-              <Link to="/yesno">
-                <TextBox>선택장애</TextBox>
-                <TextBox>Yes or No</TextBox>
-              </Link>
-            </ButtonBlock>
-            <ButtonBlock>
-              <Link to="/rouletteList">
-                <TextBox>돌려돌려</TextBox>
-                <TextBox>돌림판</TextBox>
-              </Link>
-            </ButtonBlock>
-          </ButtonLine>
-          <SearchBox>
-            <form onSubmit={onSubmit}>
-              <ButtonContainer>
-                <button type="submit" onClick={() => moveHref(data)}></button>
-                <input placeholder="식당 or 메뉴를 검색하세요!" onChange={onChange} value={input} />
-              </ButtonContainer>
-            </form>
-          </SearchBox>
-          <RoundContainer theme="image" imageLink={'https://d3ojewq8movb4o.cloudfront.net/toustous.jpg'}>
-            <a href="https://blog.naver.com/crystalnam03/222068892248">중앙대 근처 건강 웰빙 맛집 6곳</a>
-          </RoundContainer>
-          <RoundContainer theme="image" imageLink={'https://d3ojewq8movb4o.cloudfront.net/onetwothree.jpg'}>
-            서비스 준비중입니다.
-          </RoundContainer>
-          <RoundContainer theme="image" imageLink={'https://d3ojewq8movb4o.cloudfront.net/thirdbutton.jpg'}>
-            서비스 준비중입니다.
-          </RoundContainer>
-        </Container>
-        <SearchBoxContainer>
-          <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
-            <AddText>
-              <span>찾는 식당이 안보이세요?</span>
-            </AddText>
-            <button className="roundButton yHover " style={{ height: '35px', width: '100px' }}>
-              <a href="https://forms.gle/G2AGwkTyaXeN7C1T6">식당등록하기</a>
-            </button>
-          </div>
-        </SearchBoxContainer>
-      </>
-    );
-  }
+  return (
+    <>
+      <Container color="white">
+        <Header category="main" headerColor="none" withOutHeaderIcon />
+        <TitleSlogan>뭐 먹을지 고민될 땐?</TitleSlogan>
+        <TitleLogo>
+          <img src={titlelogo} alt="titlelogo" />
+        </TitleLogo>
+        <Divider></Divider>
+        <ButtonLine>
+          <ButtonBlock>
+            <Link to="/filter">
+              <TextBox>필터링</TextBox>
+              <TextBox>검색</TextBox>
+            </Link>
+          </ButtonBlock>
+          <ButtonBlock>
+            <Link to="/yesno">
+              <TextBox>선택장애</TextBox>
+              <TextBox>Yes or No</TextBox>
+            </Link>
+          </ButtonBlock>
+          <ButtonBlock>
+            <Link to="/rouletteList">
+              <TextBox>돌려돌려</TextBox>
+              <TextBox>돌림판</TextBox>
+            </Link>
+          </ButtonBlock>
+        </ButtonLine>
+        <SearchBox>
+          <form onSubmit={onSubmit}>
+            <ButtonContainer>
+              <button type="submit" onClick={() => moveHref(input)}></button>
+              <input placeholder="식당 or 메뉴를 검색하세요!" onChange={onChange} value={input} />
+            </ButtonContainer>
+          </form>
+        </SearchBox>
+        <RoundContainer theme="image" imageLink={'https://d3ojewq8movb4o.cloudfront.net/toustous.jpg'}>
+          <a href="https://blog.naver.com/crystalnam03/222068892248">중앙대 근처 건강 웰빙 맛집 6곳</a>
+        </RoundContainer>
+        <RoundContainer theme="image" imageLink={'https://d3ojewq8movb4o.cloudfront.net/onetwothree.jpg'}>
+          서비스 준비중입니다.
+        </RoundContainer>
+        <RoundContainer theme="image" imageLink={'https://d3ojewq8movb4o.cloudfront.net/thirdbutton.jpg'}>
+          서비스 준비중입니다.
+        </RoundContainer>
+      </Container>
+      <SearchBoxContainer>
+        <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
+          <AddText>
+            <span>찾는 식당이 안보이세요?</span>
+          </AddText>
+          <button className="roundButton yHover " style={{ height: '35px', width: '100px' }}>
+            <a href="https://forms.gle/G2AGwkTyaXeN7C1T6">식당등록하기</a>
+          </button>
+        </div>
+      </SearchBoxContainer>
+    </>
+  );
 }
 
-export default HomePage;
+export default withRouter(HomePage);
